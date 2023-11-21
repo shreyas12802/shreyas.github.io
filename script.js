1f055a353b8be3f8f2082d9a2b68ad0a40104b8b
shreyas12802 <72312034+shreyas12802@users.noreply.github.com>
2023-11-21 23:51:52 +0530

    added new files

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 
-        // Add this script for showing/hiding the fixed navbar
-        window.onscroll = function() {
-            var navbar = document.querySelector('.custom-nav');
-            var homeSection = document.querySelector('.home');
-            var aboutSection = document.querySelector('.about');
-                console.log('Scrolling...');
+window.onscroll = function () { scrollFunction() };
 
-            if (window.scrollY > homeSection.offsetHeight) {
-                 navbar.style.display = 'flex';
-//                 navbar.classList.add('visible');
-                } 
-            else {
-                navbar.style.display = 'none';
-//                navbar.classList.remove('visible');
-            }
-        }
-                
+function scrollFunction() {
+    const navbar = document.querySelector('.collapse-nav'); // Use class selector instead of id selector
+    var homeSection = document.querySelector('.home');
+    var aboutSection = document.querySelector('.about');
+    if (document.body.scrollTop > homeSection.offsetHeight || document.documentElement.scrollTop > homeSection.offsetHeight) {
+        navbar.style.top = "0";
+    } else {
+        navbar.style.top = "-100px";
+    }
+}
+function toggleNavbar() {
+    const verticalNavbar = document.querySelector('.collapse-nav.vertical');
+    verticalNavbar.classList.toggle('show');
+  }
+
+function maintoggleNavbar() {
+    const verticalNavbar = document.querySelector('.main-nav-vertical');
+    verticalNavbar.style.display = verticalNavbar.style.display === 'block' ? 'none' : 'block';
+//    verticalNavbar.classList.toggle('show');
+//    if (verticalNavbar.style.display="none")
+//    {verticalNavbar.style.display="block";}
+//    else if (verticalNavbar.style.display="block") {verticalNavbar.style.display="none";}
+}
